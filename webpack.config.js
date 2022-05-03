98e0a3fdc72dca8b735201f3139c58162a1423de
Ridercogswell <ridercogswell@gmail.com>
2022-05-02 19:36:12 -0700

    revert to bootstrapv4 and works

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,17 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack")
+const path = require("path");
 
-// webpack NEEDS three properties entry, output, and mode
 module.exports = {
-    // entry
-    entry: './assets/js/script.js',
-    // output
-    output: {
-        // best practice to put your bundled code into a folder named 'dist' short for distribution
-        path: path.join(__dirname + '/dist'),
-        filename: 'main.bundle.js'
-    },
-    // devtool: 'eval-cheap-source-map',
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
-    ],
-    // mode - could be production, development, etc.
-    mode: 'development'
+  entry: "./assets/js/script.js",
+  output: {
+    path: path.join(__dirname + "/dist"),
+    filename: "main.bundle.js"
+  },
+  plugins: [
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
+  ],
+  mode: "development"
 };
